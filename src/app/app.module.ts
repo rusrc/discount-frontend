cb28d42bcbfd0eebdffc9cb70283e3470244c1d3
ruslan878707@gmail.com <19871961>
2017-07-03 21:31:41 +0300

    add ngx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,36 +2,53 @@ import { AdminModule } from './admin-module/admin.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpModule, Http } from '@angular/http';
+
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 
 const routerConfig: Routes = [
-  {
-    path: '',
-    redirectTo: "home",
-    pathMatch: 'full'
-  },
-  {
-    path: "home",
-    component: HomeComponent
-  },
-  {
-    path: 'admin',
-    loadChildren: 'app/admin-module/admin.module#AdminModule'
-  }
+    {
+        path: '',
+        redirectTo: "home",
+        pathMatch: 'full'
+    },
+    {
+        path: "home",
+        component: HomeComponent
+    },
+    {
+        path: 'admin',
+        loadChildren: 'app/admin-module/admin.module#AdminModule'
+    }
 ]
 
+//https://github.com/ngx-translate/core
+export function createTranslateLoader(http: Http)
+{
+    return new TranslateHttpLoader(http, './i18n/', '.json');
+}
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent
-  ],
-  imports: [
-    BrowserModule,
-    RouterModule.forRoot(routerConfig, { enableTracing: true })
-  ],
-  providers: [],
-  bootstrap: [ AppComponent ]
+    declarations: [
+        AppComponent,
+        HomeComponent
+    ],
+    imports: [
+        BrowserModule,
+        RouterModule.forRoot(routerConfig, { enableTracing: true }),
+        TranslateModule.forRoot({
+            loader: {
+                provide: TranslateLoader,
+                useFactory: (createTranslateLoader),
+                deps: [ Http ]
+            }
+        })
+    ],
+    providers: [],
+    bootstrap: [ AppComponent ]
 })
 export class AppModule { }
